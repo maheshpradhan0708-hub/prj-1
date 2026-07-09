28382d8e09bdc5d625c8df5f779720e4697d675d
maheshpradhan0708-hub <maheshpradhan0708@gmail.com>
2026-07-09 11:17:25 +0530

    Add files via upload

---
+++ py pratice.py
@@ -0,0 +1,33 @@
+
+print("********************************************")
+
+print("welcome to the persnoal data collector")
+
+print("********************************************")
+
+Name = input('Enter the name:')
+Age = int(input('Enter your age:')) 
+Height=float(input('Enter your height:'))
+Favourite_Number=float(input('enter the number: '))
+
+Birth_year = 2026 - Age
+
+print("Your Birth Year Is :", Birth_year)
+
+print("Name:", Name," | Data Type", type (Name), " | Memory Address", id (Name))
+
+print("Age:", Age,"  | Data Type ", type (Age)," | Memory Address:", id (Age))
+
+print("Height: ", Height," | Data Type ", type (Height)," | Memory Address:" , id (Height))
+
+print("Favourite Number: ", Favourite_Number," | Data Type ", type (Favourite_Number), " | Memory Address", id (Favourite_Number))
+
+print("Your Birth Year Is", Birth_year," | Data Type ", type (Birth_year)," | Memory Address:", id (Birth_year))
+
+
+
+print("_._._._._._._._._._._._._._._._.")
+
+print("THANKYOU FOR VISITING")
+                
+print("_._._._._._._._._._._._._._._._.")
